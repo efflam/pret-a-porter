f1a0d6fc8014ec94ad90be7da68ca97956891675
Efflam <Efflam.DANIEL@publicis-modem.fr>
2017-03-10 13:45:15 +0100

    combine rules by returning array in css prop

--- src/box.js
+++ src/box.js
@@ -1,13 +1,17 @@
 import { h } from "preact";
-import computeStyle from './compute-style';
+import computeStyle from "./compute-style";
+import { combineRules } from "fela";
+
+const wrap = x => typeof x === "function" ? x : () => x;
 
 const Box = ({ as = "div", css, ...props }, { renderer, theme }) => {
   const [style, restProps] = computeStyle(theme, props);
-  const className = renderer.renderRule(() => ({
-    ...style,
-    ...(css && (typeof css === "function" ? css(theme, style) : css))
-  }));
-  return h(as, {className, ...restProps})
+  const cssRule = css && Array.isArray(css)
+    ? combineRules(...css.map(rule => wrap(rule)))
+    : wrap(css);
+  const rule = combineRules(wrap(style), cssRule);
+  const className = renderer.renderRule(rule, theme);
+  return h(as, { className, ...restProps });
 };
 
 export default Box;
