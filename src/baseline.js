0f4ec492ab342bbc7f1f9817bab337dc2f26ab44
Efflam <Efflam.DANIEL@publicis-modem.fr>
2017-03-21 20:52:05 +0100

    clean grid

--- src/baseline.js
+++ src/baseline.js
@@ -5,7 +5,7 @@ export default ({ color = "#666", opacity = 0.2, ratio = 1 }, { theme }) => {
     <div
       style={{
         backgroundImage: `linear-gradient(${color} 1px, transparent 1px)`,
-        backgroundSize: `auto ${theme.typography.rhythm(ratio)}`,
+        backgroundSize: `auto ${theme.typography.rhythm(ratio)}px`,
         marginTop: -1,
         pointerEvents: "none",
         position: "absolute",
--- src/grid.js
+++ src/grid.js
@@ -1,20 +1,20 @@
 import { h, Component } from "preact";
 import Box from "./box";
 
-export class Grid extends Component {
+class Grid extends Component {
   getChildContext() {
     return {
       gutter: this.props.gutter
     };
   }
-
-  render() {
-    const { gutter, cellStyle, children, ...props } = this.props;
-    const mx = Array.isArray(gutter) ? gutter.map(g => (-g) / 2) : (-gutter) / 2;
+  render({ gutter, ...props }) {
     return (
-      <Box display="flex" flexWrap="wrap" marginHorizontal={mx} {...props}>
-        {children}
-      </Box>
+      <Box
+        display="flex"
+        flexWrap="wrap"
+        mx={Array.isArray(gutter) ? gutter.map(g => -g / 2) : -gutter / 2}
+        {...props}
+      />
     );
   }
 }
@@ -23,19 +23,15 @@ Grid.defaultProps = {
   gutter: 0
 };
 
-export class Cell extends Component {
-  render({ width = "100%", style, ...props }, state, { gutter, cellStyle }) {
-    const px = Array.isArray(gutter) ? gutter.map(g => g / 2) : gutter / 2;
-    return (
-      <Box
-        width={width}
-        style={theme => ({
-          ...(style && style(theme)),
-          ...(cellStyle && cellStyle(theme))
-        })}
-        {...props}
-        paddingHorizontal={px}
-      />
-    );
-  }
-}
+const Cell = (props, { gutter }) => (
+  <Box
+    {...props}
+    px={Array.isArray(gutter) ? gutter.map(g => g / 2) : gutter / 2}
+  />
+);
+
+Cell.defaultProps = {
+  width: "100%"
+};
+
+export { Grid, Cell };
